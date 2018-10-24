8033a02fd5fcc24d4d70e836fab6e146fe1bb33a
Mathias Hermansson <mathias.hermansson@se.ibm.com>
2018-10-24 19:48:23 +0200

    Remove some outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,4 @@
 
-output "icp_public_key" {
-  description = "The public key used for boot master to connect via ssh for cluster setup"
-  value = "${var.generate_key ? tls_private_key.icpkey.public_key_openssh : var.icp_pub_key}"
-}
-
-output "icp_private_key" {
-  description = "The public key used for boot master to connect via ssh for cluster setup"
-  value = "${var.generate_key ? tls_private_key.icpkey.private_key_pem : var.icp_priv_key}"
-}
-
 output "install_complete" {
   depends_on  = ["null_resource.icp-install"]
   description = "Boolean value that is set to true when ICP installation process is completed"
